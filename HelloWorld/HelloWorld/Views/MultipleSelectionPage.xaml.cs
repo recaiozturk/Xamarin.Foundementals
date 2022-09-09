7da3fe6a99b38d9d9a492bfa8e705763cd23668a
Recai <recaiozturk54@gmail.com>
2022-09-09 16:18:30 +0300

    Added SingleSelectionPage with CollectionView

---
+++ HelloWorld/HelloWorld/Views/MultipleSelectionPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MultipleSelectionPage : ContentPage
+    {
+        public MultipleSelectionPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    private void movieList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var previous = e.PreviousSelection;
+        var current = e.CurrentSelection;
+
+        
+
+        for (int i = 0; i < previous.Count; i++)
+        {
+            lblPrevious.Text += (previous[i] as Movie)?.Name + " ";
+        }
+
+        for (int i = 0; i < current.Count; i++)
+        {
+            lblCurrent.Text += (current[i] as Movie)?.Name + " ";
+        }
+
+    }
+
+        
+        
+}
---
+++ HelloWorld/HelloWorld/Views/SingleSelectionPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using HelloWorld.Models;
+using HelloWorld.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SingleSelectionPage : ContentPage
+    {
+        public SingleSelectionPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MovieViewModel();
+        }
+
+        private void movieList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var previous = (e.PreviousSelection.FirstOrDefault() as Movie)?.Name; //previous secim bos değil ise  Name Property sini  değişkene at
+            var current = (e.CurrentSelection.FirstOrDefault() as Movie)?.Name;
+
+            lblCurrent.Text = current;
+            lblPrevious.Text = previous;
+        }
+    }
+}
